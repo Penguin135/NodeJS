e7a3071c79aa865a1c8df43f1d9f35629aed1e33
Penguin135 <rudwns273@naver.com>
2020-10-15 21:05:00 +0900

    메일 전송 완성(naver)

--- main-nodemailer.js
+++ main-nodemailer.js
@@ -1,1 +1,31 @@
-repuir
+var nodemailer = require('nodemailer');
+var mailer = require('./config/nodemailer.json');
+
+function sendEmail(toEmail, title, txt){
+    var transporter = nodemailer.createTransport({
+        host: mailer.smtpServerURL, // SMTP 서버 주소
+        secure: true,               // false로 사용시, port 옵션 추가 필요
+        auth:{
+            user: mailer.authUser,  // SMTP 메일서버 계정
+            pass: mailer.authPass   // SMTP 메일서버 비번
+        }
+    });
+
+    var mailOptions = {
+        from: mailer.fromEmail, // 보내는 사람 주소
+        to: toEmail,            // 받는 사람 주소
+        subject: title,         // 제목
+        text:txt               // 본문
+        // html: ``
+    };
+
+    transporter.sendMail(mailOptions, function(err, info){
+        if(err){
+            console.log(err);
+        }
+        console.log("sended email" + info.response);
+        transporter.close();
+    })
+}
+
+sendEmail('rudwns273@naver.com', 'hello', 'Hello World');
