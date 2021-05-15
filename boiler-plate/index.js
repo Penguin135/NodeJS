4b67a6c8743af81281861bce575edd1b7b5f43e5
Penguin135 <rudwns273@naver.com>
2021-05-15 18:58:26 +0900

    add login function with token

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,33 +2,61 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const cookieParser = require('cookie-parser')
+
 const config = require('./config/key')
 
 const { User } = require('./models/User')
 
 const mongoose = require('mongoose')
+const { response } = require('express')
 mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(()=>console.log('MongoDB Connected...'))
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(cookieParser())
 app.get('/', (req, res) => {
   res.send('Hello World!!')
 })
 
-app.post('/register', (req, res)=>{
+app.post('/register', (req, res) => {
   // get information from clinet, insert infromation into database
   const user = new User(req.body)
-  user.save((err, userInfo)=>{
-    if(err) return res.json({success: false, err})
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
     return res.status(200).json({
       success: true
     })
   })
+})
 
+app.post('/login', (req, res) => {
+  // 1. find email in database
+  User.findOne({ email: req.body.email }, (err, user) => {
+    console.log(user)
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: 'no matched email'
+      })
+    }
+    // 2. if email exist in database, confirm password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) return res.json({ loginSuccess: false, message: 'password is not correct' })
+      // 3. if pass word is matched, create token
+      user.genToken((err, user) => {
+        if (err) return res.status(400).send(err)
+
+        // save token to cookie
+        res.cookie('x_auth', user.token)
+          .status(200)
+          .json({ loginSuccess: true, userid: user._id })
+      })
+    })
+  })
 })
 
 app.listen(port, () => {
--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
 
+var jwt = require('jsonwebtoken');
+
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -50,6 +52,27 @@ userSchema.pre('save', function (next) {
     }
 })
 
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    // match plain password with DB
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.genToken = function(cb){
+    var user = this
+
+    // using jsonwebtoken, create token
+    // user._id + 'secretToken' = token
+    // using secretToken, identify user._id
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
