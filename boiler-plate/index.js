9cf0ab70a8f9c9415a8c3e424e381354818c85a0
Penguin135 <rudwns273@naver.com>
2021-05-15 17:38:45 +0900

    secure information-mongo

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,14 +2,33 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require('./config/key')
+
+const { User } = require('./models/User')
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kajunkim:tema474845!@boilerplate.l0o6o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=>console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!')
+})
+
+app.post('/register', (req, res)=>{
+  // get information from clinet, insert infromation into database
+  const user = new User(req.body)
+  user.save((err, userInfo)=>{
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
 })
 
 app.listen(port, () => {
